23a63272fa2d31d6899f325fe8c00d8d695bba9f
forest6511 <20209757+forest6511@users.noreply.github.com>
2025-03-10 13:34:34 +0900

    init

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,9 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 
+import partytown from '@astrojs/partytown'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), sitemap()],
+  integrations: [react(), sitemap(), partytown()],
   site: 'https://quickify.tools',
   vite: {
     plugins: [tailwindcss()],
